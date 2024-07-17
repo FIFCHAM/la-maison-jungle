c23d1c0ba8535cf622dac6e4bad50fb9b83b64cb
Fifcham <body.bertrand@gmail.com>
2024-07-17 18:30:17 +0200

    creer categories

--- src/composents/App.js
+++ src/composents/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import Banner from './Banner'
+import logo from '../assets/logo.png'
 import Cart from './Cart'
-
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
 function App() {
+    const [cart, updateCart] = useState([])
     return (
         <div>
-            <Banner /> 
-            <Cart />
+             <Banner>
+             <img src={logo} alt='La maison jungle' className='lmj-logo' />
+             <h1 className='lmj-title'>La maison jungle</h1>
+                </Banner> 
+                <div className='lmj-layout-inner'>
+            <Cart cart={cart} updateCart={updateCart} />
+            <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+            
     
         </div>
 
